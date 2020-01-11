e3b6ad11d1fd94237f5d6c19e758b2941adf469b
francescocretti <francesco.cretti@gmail.com>
2020-01-11 20:40:23 +0100

    alpha version

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
-console.log('load header');
+const scrollToElement = (elementId, offset) => {
+  $("html, body").animate({
+    scrollTop: $(`${elementId}`).offset().top + (offset || 0)
+  }, 800);
+};
+
 
 $('#header').load('/template/header.html', () => {
-  console.log('header laoded');
+  $('.nav-link').click(e => {
+    e.preventDefault();
+    const targetSection = e.currentTarget.hash;
+    scrollToElement(e.currentTarget.hash, -80);
+  });
 });
+
+$('#footer').load('/template/footer.html');
