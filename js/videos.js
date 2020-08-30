b600d697a5b68167f219deb8233297f7549c69b2
francescocretti <francesco.cretti@gmail.com>
2020-08-30 13:41:07 +0200

    use Vimeo embed on iPhone

--- js/videos.js
+++ js/videos.js
@@ -1,6 +1,10 @@
-document.querySelectorAll('.presentation-col').forEach(col => {
-  const img = col.querySelector('.presentation-image');
-  const video = col.querySelector('.presentation-video');
+const iOSSafari = () => {
+  return /iP(ad|od|hone)/i.test(navigator.userAgent) && /WebKit/i.test(navigator.userAgent) && !(/(CriOS|FxiOS|OPiOS|mercury)/i.test(navigator.userAgent));
+};
+
+const setHtml5Player = container => {
+  const img = container.querySelector('.presentation-image');
+  const video = container.querySelector('.presentation-video-html5');
 
   img.addEventListener('click', e => {
     $(img).fadeOut(600, () => {
@@ -16,4 +20,28 @@ document.querySelectorAll('.presentation-col').forEach(col => {
       });
     });
   });
+};
+
+const setVimeoPlayer = container => {
+  const img = container.querySelector('.presentation-image');
+  const video = container.querySelector('iframe');
+  const player = new Vimeo.Player(video);
+
+  img.addEventListener('click', e => {
+    $(img).fadeOut(600, () => {
+      $(video).fadeIn(500, () => {
+        player.play();
+        const backToImg = () => {
+          $(video).fadeOut(400, () => $(img).fadeIn());
+          player.pause();
+          player.setCurrentTime(0);
+        };
+        player.on('ended', backToImg);
+      });
+    });
+  });
+};
+
+document.querySelectorAll('.presentation-col').forEach(col => {
+  iOSSafari() ? setVimeoPlayer(col) : setHtml5Player(col);
 });
